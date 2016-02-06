ad131a48462b02dc9746a8c789eb0a968f31fadb
Bilkit <githinjibcw@hotmail.com>
2016-02-05 17:47:38 -0800

    fit all points

--- scripts/plotTrials.py
+++ scripts/plotTrials.py
@@ -14,33 +14,30 @@ def plot(traj,env,targ_file):
    # # won't plot because of index?
    # pts.plot(kind = 'scatter');
 
+   # zoom to fit trees & traj
+   # min/max of points
+
+
    ax = plt.figure().add_subplot(111)
 
    print("plottig pts: "+str(len(traj)))
    ax.scatter(traj.pos_x,traj.pos_y,s=5,c='b',marker='.')
 
    print("plottig trees: "+str(len(env)))
-
-   # save old header
-   # toadd = pd.DataFrame([[oldlabels[0],oldlabels[1],oldlabels[2]]],columns=['pos_x','pos_y','radius'])
-   # change column labels
-   # dtcopy.columns = toadd.columns
-   # dt.append(toadd)
-   print(len(env))
-   toappend = pd.DataFrame([[env.columns[0],env.columns[1],env.columns[2]]],
-      columns=['pos_x','pos_y','radius'])
-   env.columns = toappend.columns
-   env.append(toappend,ignore_index=True)
-   print(len(env))
-
+   # save tree in header
+   ax.add_patch(plt.Circle((int(float(env.columns[0]))
+      ,int(float(env.columns[1])))
+      ,int(float(env.columns[2]))
+      ,color='g'))
+   env.columns = ['pos_x','pos_y','radius']
    for tree in env.values:
       ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
-   # ax.scatter(env.pos_x,env.pos_y,s=10,c='g',marker='o')
 
    plt.title(traj.moth_id.iloc[0]+" in "+traj.obstacles.iloc[0]+" forest")
    plt.xlabel("x")
+   plt.xlim(min(min(env.pos_x),min(traj.pos_x)),max(max(env.pos_x),max(traj.pos_x)))
    plt.ylabel("y")
-
+   plt.ylim(min(min(env.pos_y),min(traj.pos_y)),max(max(env.pos_y),max(traj.pos_y)))
 
    plt.savefig(targ_file)
    return
