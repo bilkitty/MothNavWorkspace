3405030810551389da9c24c82c442991145401cb
Bilkit <githinjibcw@hotmail.com>
2016-02-17 17:01:17 -0800

    fixed visualization bug: use dict (index:1_or_0) to keep track of undrawn trees

--- scripts/collisions.py
+++ scripts/collisions.py
@@ -9,16 +9,7 @@ R_CLOSECALL = 10; # distance (in Rmax units) from tree edge
 #MOTH_RAD = 0; # moth radius is negl. small compared to tree rad
 
 # returns count of close calls and collisions per point
-def detect_closecalls(pm,patch,rmax,ax):
-   # if you drew a line connecting moth and tree
-   # consider these points to lie on the line
-   # D = d^1/2
-   # check D(tc,mc) < R_CLOSECALL
-   #   check if collision (i.e. D(tc,mc) <= t_rad)
-   #     count collision
-   #   else
-   #     count close
-
+def analyse_patch(pm,patch,rmax,ax):
    collision = 0;
    close = 0;
 
@@ -27,42 +18,37 @@ def detect_closecalls(pm,patch,rmax,ax):
       dd = (tn[0] - pm[0])**2 + (tn[1] - pm[1])**2
       dd = dd**0.5
 
+      # test for collision or close call
       if dd < R_CLOSECALL*rmax:
          if dd < tn[2]:
             collision +=1
+      #-- DEBUG
             ax.add_patch(plt.Circle((tn[0]
                ,tn[1])
                ,5
                ,color='r'))
+      #-- DEBUG
          else:
             close +=1
 
-
    return [close,collision]
 
-# returns count of collisons and close calls
 # walks along trajectory accumulating collision
 # count and close callcount (later score)
 def count_collisions_closecalls(traj,env):
-   # get tree radius max
-   #Rmax = max(dt.r)
-   # get points (x,y,hx,hy)
-   #pm11 = m11[['pos_x','pos_y','head_x','head_y']]
-
-   # for all points: # pn = pm11.values[n:n+1][0]
-   #   for all trees:
-   #     check if inside scope = sq(2*tree radius max)
-   #        if( detect_collision ):
-   #           count collision
-   #        else:
-   #           count closecall
-
    print("INFO: detecting close calls within "+str(PATCH_SIZE)+"*Rmax of traj pt")
 
-   total_score = [0,0]
+   # determines size of patch (aka search space)
    Rmax = max(env.r)
+   # keep close call and collision count
+   total_count = [0,0]
+   # initialize dict of tree indices and binary value
+   # representing whether it has been drawn
+   undrawn_trees = {}
+   for ii in env.index:
+      undrawn_trees[ii] = 1
+   # isolate (x,y) and <hx,hy> info from traj data
    points = traj[['pos_x','pos_y','head_x','head_y']]
-   undrawn_trees = env.index
 
    #-- DEBUG
    ax = plt.figure().add_subplot(111)
@@ -78,42 +64,48 @@ def count_collisions_closecalls(traj,env):
       # get trees within patch
       tclose = env[l_cutoff & r_cutoff & u_cutoff & b_cutoff]
       #-- DEBUG
-      # if len(tclose) > 0:
-      #    print("p: ("+str(pn[0])+','+str(pn[1])+')')
-      #    print("tclose: "+str(len(tclose)))
-
       # draw trees within patch bruh
       for tn in tclose.index:
          # print("drawing close trees...")
-         if tn in undrawn_trees.values:
-            ax.add_patch(plt.Circle((tclose.loc[tn][0]
-               ,tclose.loc[tn][1])
-               ,tclose.loc[tn][2]
-               ,color='g'))
-            undrawn_trees = undrawn_trees.delete(tn)
+         if undrawn_trees[tn] == 1:
+            ax.add_patch(plt.Circle((env.loc[tn][0]
+               ,env.loc[tn][1])
+               ,env.loc[tn][2]
+               ,color='c'))
+            undrawn_trees[tn] = 0
 
       # draw traj point
       ax.add_patch(plt.Circle((pn[0],pn[1]),.05,color='b'))
       # -- DEBUG
 
       # check for close call or collision
-      score = detect_closecalls(pn,tclose,Rmax,ax)
-      total_score[0] += score[0]
-      total_score[1] += score[1]
+      count = analyse_patch(pn,tclose,Rmax,ax)
+      total_count[0] += count[0]
+      total_count[1] += count[1]
 
 
-   print("closecalls: "+str(total_score[0])+" collision: "+str(total_score[1]))
+   print("closecalls: "+str(total_count[0])+" collisions: "+str(total_count[1]))
 #-- DEBUG
    # draw remaining trees
-   for tn in undrawn_trees.values:
-      ax.add_patch(plt.Circle((env.loc[tn][0]
-         ,env.loc[tn][1])
-         ,env.loc[tn][2]
-         ,color='g'))
+   for tn in undrawn_trees.keys():
+      if(undrawn_trees[tn] == 1):
+         ax.add_patch(plt.Circle((env.loc[tn][0]
+            ,env.loc[tn][1])
+            ,env.loc[tn][2]
+            ,color='g'))
+         undrawn_trees[tn] = 0
 
-   plt.xlim(min(env.x),max(env.x))
-   plt.ylim(min(env.y),max(env.y))
-   plt.show()
+   plt.xlim(min(min(env.x),min(traj['pos_x']))
+      ,max(max(env.x),max(traj['pos_x'])))
+   plt.ylim(min(min(env.y),min(traj['pos_y']))
+      ,max(max(env.y),max(traj['pos_y'])))
+   # plt.show()
+
+   # prompt to save figure
+   yes_or_no = input("save figure? (yes/no)\n")
+   if(yes_or_no.lower() == "yes"):
+      target_file = input("filename: ")
+      plt.savefig(target_file)
 #-- DEBUG
 
    return
