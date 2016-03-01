6198e3b4ee0e8c07e7ef60fb4f18b84f2c902740
Bilkit <githinjibcw@hotmail.com>
2016-03-01 12:09:32 -0800

    added functions: get_patch and map_to_index for cleaner code. also added comments

--- scripts/score.py
+++ scripts/score.py
@@ -3,32 +3,31 @@
 from plotTrials import plot_frame
 from math import floor
 import numpy as np
+import pandas as pd
 
 PAD = 0  # pad patch for discritization
 
-# generate score for single traj point
-
-# args:
-# traj point
-# tree patch (floor(x2) - floor(x1))
-# smallest tree rad out of forest
-
-# return:
-# integer value for traj point
-
-# steps:
-# generate MxM mask of trees where M = dx/(Rmin/2)
-# generate some kernel MxM
-# dot the mask and kernel
 def score(pt,patch,size,rmin):
    ret = 0 # convolution result
 
    return ret
 
-def get_center_block():
-   return
+# computes matrix indices as ith-block and
+# jth-block between tcenter and origin.
+def map_to_mat_idx(tcenter,orig,bsize):
+   # get deltax, deltay of tree/moth
+   tx2px = tcenter[0] - orig.pos_x
+   ty2py = tcenter[1] - orig.pos_y
+   # get half blocks between mcenter and tcenter
+   ihalf = int( 2*tx2px/bsize ) # cols are x
+   jhalf = int( 2*ty2py/bsize ) # rows are y
+   # convert to index by combining halves
+   ii = ihalf if abs(ihalf) < 2 else int(ihalf/2)
+   jj = jhalf if abs(jhalf) < 2 else int(jhalf/2)
 
-# Divide patch into min tree radius/2 sized
+   return (ii,jj)
+
+# divide patch into min tree radius/2 sized
 # blocks and bin tree points into a matrix
 # MxM.
 # ARGS: moth point, tree patch, patch size,
@@ -36,8 +35,18 @@ def get_center_block():
 # RETURNS: matrix MxM, block size, where
 #   M = 2*patch/block size (odd)
 def discretize(pt,patch,sz,rmin):
-  # test with large blocks first
+  print("discretizing")
+  if(not isinstance(patch,pd.DataFrame)):
+     print("  (!) patch is not data frame")
+     return None
+  print("  patch contains: "+str(len(patch)))
+  print("  patch size: "+str(sz))
+
+  # get block size
   SZb = rmin/2
+  print("  blocksize: "+str(SZb))
+
+  # initialize matrix
   Nb = int(2*sz/SZb)
   # make sure matrix is oddxodd
   Nb += (Nb+1)%2
@@ -49,29 +58,37 @@ def discretize(pt,patch,sz,rmin):
   cnt = 0 #debug
   for tt in patch.values:
      print("tree:"+str(cnt))
-     # get deltax, deltay of tree/moth
-     tx2px = tt[0] - pt.pos_x
-     ty2py = tt[1] - pt.pos_y
-     print("dx="+str(ty2py)+", dy="+str(tx2px))
-     # get half blocks between mcenter and tcenter
-     ihalf = int( 2*tx2px/SZb ) # cols are x
-     jhalf = int( 2*ty2py/SZb ) # rows are y
-     print("ih="+str(ihalf)+", jh="+str(jhalf))
-     bi = ihalf if abs(ihalf) < 2 else int(ihalf/2)
-     bj = jhalf if abs(jhalf) < 2 else int(jhalf/2)
+     itt = map_to_mat_idx(tt,pt,SZb)
 
-     btt = (bi,bj)
-     print("bi="+str(bi)+", bj="+str(bj))
+     Mi = (Nb/2)+itt[0];
+     Mj = (Nb/2)+itt[1];
+
+     # view error in reconstructing xy distance b/w tree and moth center
+     print("  ii="+str(itt[0])+", jj="+str(itt[1]))
+     print("  tx="+str(tt[0])+"~ii*bsz+px="+str(itt[0]*SZb+pt.pos_x))
+     print("  ty="+str(tt[1])+"~jj*bsz+px="+str(itt[1]*SZb+pt.pos_y))
 
      # indicate tree center in matrix index
-     Bi = btt[0];
-     Bj = btt[1];
-     mat[(Nb/2)+Bi][(Nb/2)+Bj] = cnt+1 # show which tree is where
+     mat[Mi][Mj] = cnt+1 # show which tree is where
      # mat[(Nb/2)+Bi][(Nb/2)+Bj] = 1
 
      cnt += 1
 
   return [mat,SZb]
+
+# generates data frame slice of env objects
+# contained within a square around origin.
+# ARGS: origin (to center patch on), forest
+# RETURNS: tree patch, patch size (radius not
+#   number of trees)
+def get_patch(orig,env):
+   patch_size = (int)(50*max(env.r)/2) + PAD
+   l = orig[0]-patch_size < env.x+env.r
+   r = env.x-env.r < orig[0]+patch_size
+   u = env.y-env.r < orig[1]+patch_size
+   d = orig[1]-patch_size < env.y+env.r
+   patch = env[l & r & u & d]
+   return [patch, patch_size]
 
 # sets up tree patch centered on single point
 # from moth traj and tree data.
@@ -82,17 +99,14 @@ def setup_test(md,td,display=False):
    # get a single point
    xypoint = md.loc[0] #(int)((len(md)-1)/2)] # midpoint
    xy = xypoint[['pos_x','pos_y']]
+   print("centering around point: ("+str(xy.pos_x)+","+str(xy.pos_y)+")")
 
-   # get patch around single point
+   # get patch centered on moth point
    # size is L/2 for patch LxL
-   patch_size = (int)(50*max(td.r)/2) + PAD
-   l = xy[0]-patch_size < td.x+td.r
-   r = td.x-td.r < xy[0]+patch_size
-   u = td.y-td.r < xy[1]+patch_size
-   d = xy[1]-patch_size < td.y+td.r
-   patch = td[l & r & u & d]
+   [patch,patch_size] = get_patch(xy,td)
+
    # visualize patch
    if(display):
-      plot_frame(xypoint,patch,2*patch_size,td)
+      plot_frame(xypoint,patch,patch_size,td)
 
    return [xy,patch,patch_size]
