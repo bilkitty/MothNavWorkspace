fe6bceb9ab224469484d8ff9eeb454d77614912a
Bilkit <githinjibcw@hotmail.com>
2016-03-01 12:08:09 -0800

    cleanup and comment

--- scripts/plotTrials.py
+++ scripts/plotTrials.py
@@ -3,20 +3,27 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 
-# Displays plot if no filename is
+# displays plot if no filename is
 # specified otherwise save plot
 # using file arg
 # ARGS: plot, filename
 # RETURNS: void
-def visualize(plt,file):
-   if(targ_file == None):
+def visualize(plt,filename):
+   if(filename == None):
       plt.show()
    else:
-      plt.savefig(targ_file)
+      plt.savefig(filename)
 
    plt.close();
    return
 
+# displays plot of non-zero values
+# in matrix where each index represents
+# a block of size bsz. Please pass a
+# sparse matrix.
+# ARGS: matrix (sparse), block size,
+#   filename (optional)
+# RETURNS: void
 def plot_mat(mat,bsz,targ_file=None):
    ax = plt.figure().add_subplot(111)
    ax.set_axis_bgcolor('black');
@@ -25,6 +32,7 @@ def plot_mat(mat,bsz,targ_file=None):
    szx = mat.shape[0]
    szy = mat.shape[1]
 
+   print("plotting mat("+str(szx)+"x"+str(szy)+")")
    for row in range(0,szx):
       for col in range(0,szy):
          if mat[row][col] != 0:
@@ -33,7 +41,7 @@ def plot_mat(mat,bsz,targ_file=None):
             else:
                ax.scatter(row,col,s=50,c='r',marker='o')
 
-   plt.title("kernel (block_size="+str(bsz)+")")
+   plt.title("kernel (block_size="+str(round(bsz,5))+")")
    plt.xlabel("discritized x")
    plt.xlim(-1,szx)
    plt.ylabel("discritized y")
@@ -42,14 +50,19 @@ def plot_mat(mat,bsz,targ_file=None):
    visualize(plt,targ_file)
    return
 
+# displays objects in environment and
+# trajectory path. Pass a filename to
+# save the figure.
+# ARGS: trajectory, environment, file (opt)
+# RETURNS: void
 def plot(traj,env,targ_file=None):
    ax = plt.figure().add_subplot(111)
 
-   print("plottig trees: "+str(len(env)))
+   print("plotting trees: "+str(len(env)))
    for tree in env.values:
       ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
 
-   print("plottig pts: "+str(len(traj)))
+   print("plotting pts: "+str(len(traj)))
    ax.scatter(traj.pos_x,traj.pos_y,s=5,c='b',marker='.')
 
    if('obstacles' in traj.columns):
@@ -68,6 +81,14 @@ def plot(traj,env,targ_file=None):
    visualize(plt,targ_file)
    return
 
+# displays objects in environment, patch,
+# and trajectory point. A box of length
+# 2*patch size is drawn to visually indicate
+# patch boundary. Pass a filename to save
+# the figure.
+# ARGS: point, patch, patch size, env
+#  filename (opt)
+# RETURNS: void
 def plot_frame(pt,patch,size,env,targ_file=None):
    ax = plt.figure().add_subplot(111)
 
@@ -77,7 +98,7 @@ def plot_frame(pt,patch,size,env,targ_file=None):
       if(not(tree[0] in patch.x and tree[1] in patch.y and tree[2] in patch.r)):
          ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
 
-   print("plotting point and patch: "+str(len(patch)))
+   print("plotting patch trees: "+str(len(patch)))
    ax.scatter(pt.pos_x,pt.pos_y,s=100*max(env.r),c='b',marker='x')
    # plot trees in patch
    for tree in patch.values:
