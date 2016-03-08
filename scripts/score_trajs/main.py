bd74a22c0a966c1c50decead9688b5db525d5b11
Bilkit <githinjibcw@hotmail.com>
2016-03-07 17:01:29 -0800

    forgot to move tests before prints

--- scripts/score_trajs/main.py
+++ scripts/score_trajs/main.py
@@ -7,15 +7,16 @@ import time
 def main():
    start = time.time()
    # read moth and tree data
-   dtree = load_data("csv","../data/test/csv/forest.csv")
-   dmoth = load_data("csv","../data/test/csv/moth6_single.csv")    # check for loaded files
-   print( "Processing points: "+str(len(dmoth.values)) )
-   print( "Forest size: "+str(len(dtree.values)) )
+   dtree = load_data("csv","../../data/test/csv/forest.csv")
+   dmoth = load_data("csv","../../data/test/csv/moth6_single.csv")    # check for loaded files
 
    # check that moth and tree data are not empty
    if(len(dtree)+len(dmoth) == 0):
       printf("(!) ERROR: No data loaded.")
       return
+
+   print( "Processing points: "+str(len(dmoth.values)) )
+   print( "Forest size: "+str(len(dtree.values)) )
 
    score.walk(dmoth,dtree,display=False)
    print("Total CPU Time (s): "+str(round(time.time() - start,5)))
