ab10362d5be832bd58e878ff78028beb0d79c172
Bilkit <githinjibcw@hotmail.com>
2016-03-31 13:23:20 -0700

    discritized frames from each trajectory will be loaded instead of generated.

--- scripts/score.py
+++ scripts/score.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
-from plotStuff import plot_frame, plot_mat
-from discretize import discretize
+from plotStuff import plot_frame,
+   plot_mat
 import numpy as np
 import pandas as pd
 import time
 import sys
 
 PAD = 10  # pad patch for discritization
+MASK_FILEPATH = "./discretized/"
 
 def is_square_mat(mat):
    if(mat.shape[0] == 0 or mat.shape[1] == 0):
@@ -84,61 +85,79 @@ def walk(dm, td, ktype='uniform', display=False):
    cummulative_score = 0
    min_score = sys.maxsize
    max_score = -sys.maxsize - 1
-   # get moth xy data from data frame
-   dm = dm[['pos_x','pos_y']]
+   # # get moth xy data from data frame
+   # dm = dm[['pos_x','pos_y']]
    # make sure there are data in moth data frame
-   if(len(dm.values) == 0 or len(dm.values[0]) != 2):
-      print("(!) Cannot process traj data of size:"+str(len(dm.values))\
-         +" & len:"+str(len(dm.values[0])))
-      return 1
-   if(len(td.values) == 0 or len(td.values[0]) != 3):
-      print("(!) Cannot process tree data of size:"+str(len(td.values))\
-         +" & len:"+str(len(td.values[0])))
-      return 1
+#   if(len(dm.values) == 0 or len(dm.values[0]) != 2):
+#      print("(!) Cannot process traj data of size:"+str(len(dm.values))\
+#         +" & len:"+str(len(dm.values[0])))
+#      return 1
+#   if(len(td.values) == 0 or len(td.values[0]) != 3):
+#      print("(!) Cannot process tree data of size:"+str(len(td.values))\
+#         +" & len:"+str(len(td.values[0])))
+#      return 1
 
    # measure processing times
-   # 0 = get_patch, 1 = discritize, 2 = score_frame
+   # 0 = load discretized frames, 1 = score_frame
    procTime = [0.,0.,0.]
    cnt = 1
    score_cnt = 0
-   # get mask of first point
-   start = time.time()
-   [patch,sz] = get_patch(dm.loc[0],td)
-   procTime[0] += time.time() - start
 
-   start = time.time()
-   [mask, bsize] = discretize(dm.loc[0],patch,sz,min(td.r))
-   procTime[1] += time.time() - start
+   # load discretized frames for each trajectory
+   # from pickled dict
+   loaded_frames = [np.ones((size,size),dtype=int)]
+   size = 1
+   mask = loaded_frames[0]
+
+#   # get mask of first point
+#   start = time.time()
+#   [patch,sz] = get_patch(dm.loc[0],td)
+#   procTime[0] += time.time() - start
+
+#   start = time.time()
+#   [mask, bsize] = discretize(dm.loc[0],patch,sz,min(td.r))
+#   procTime[1] += time.time() - start
+
+#   # save mask
+#   # later include timestamp
+#   save_as_sparse(mask,MASK_FILEPATH+"m"+str(cnt)+".mtx")
+
    # initialize kernel
    kernel = generateKernel(ktype,mask.shape[0])
-   # initialize score
-   if(is_square_mat(mask) and is_square_mat(kernel)):
-      start = time.time()
-      score = score_frame(mask,kernel)
-      if(score < min_score): min_score = score
-      if(max_score < score): max_score = score
-      cummulative_score += score
-      procTime[2] += time.time() - start
-      score_cnt += 1
-   else:
-      print("(!) Walk: Either mask or kernel is not square")
+#   # initialize score
+#   if(is_square_mat(mask) and is_square_mat(kernel)):
+#      start = time.time()
+#      score = score_frame(mask,kernel)
+#      if(score < min_score): min_score = score
+#      if(max_score < score): max_score = score
+#      cummulative_score += score
+#      procTime[2] += time.time() - start
+#      score_cnt += 1
+#   else:
+#      print("(!) Walk: Either mask or kernel is not square")
 
    if(display):
       plot_mat(mask,bsize,"initial_mask.png")
 
+
+   # load discretized frames of trajectory
+   for mask in loaded_frames:
    # process other points
-   for point in dm.values[1:10]:
-      cnt += 1
-      print("pt "+str(cnt)+" ("+str(round(point[0],3))+","+str(round(point[1],3))+"):",end='')
-      # get scoring region, may contain trees
-      start = time.time()
-      [patch,sz] = get_patch(point,td)
-      procTime[0] += time.time() - start
-      print("\t"+str(len(patch.values))+" ts")
-      # discretize that shit
-      start = time.time()
-      [mask, bsize] = discretize(point,patch,sz,min(td.r))
-      procTime[1] += time.time() - start
+   # for point in dm.values[1:10]:
+      # cnt += 1
+      # print("pt "+str(cnt)+" ("+str(round(point[0],3))+","+str(round(point[1],3))+"):",end='')
+      # # get scoring region, may contain trees
+      # start = time.time()
+      # [patch,sz] = get_patch(point,td)
+      # procTime[0] += time.time() - start
+      # print("\t"+str(len(patch.values))+" ts")
+      # # discretize that shit
+      # start = time.time()
+      # [mask, bsize] = discretize(point,patch,sz,min(td.r))
+      # procTime[1] += time.time() - start
+      # # save mask
+      # # later include timestamp
+      # save_as_sparse(mask,MASK_FILEPATH+"m"+str(cnt)+".mtx")
 
       # update score
       if(is_square_mat(mask) and is_square_mat(kernel)):
