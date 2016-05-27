9d6abf45ffbe0908ebd9339572333ea0725b9c8a
Bilkit <githinjibcw@hotmail.com>
2016-05-27 10:39:04 -0700

    added main function skeleton

--- scripts/randomForests.py
+++ scripts/randomForests.py
@@ -3,14 +3,36 @@
 from loadYoyoData import load_data
 from plotStuff import plot_scores, plot_mat
 import numpy as np
+import pandas as pd
 
-def createNewForest():
+
+def createNForests():
+  """
+  (str,int,str) -> None
+
+  Given a source file path to the seed forest, create N forests and
+  save them as pandas dataframes in a destination path. The destination
+  path is the current directory by default and "dest" otherwise.
+  """
+
+  # load tree data
+
+  # loop over n:
+  #   initialize dataframem (?)
+  #   newForest
+  #   convert to dataframe
+  #   save as csv in dest path
+
+  return
+
+
+def newForest():
   """
   (pandas.dataframe) -> (ndarray)
 
   Given a pandas data frame of forest data, reproduce a
   new set of forest data sample_size trees.
-  >>> createNewForest(loaded_forest_data,sample_size)
+  >>> newForest(loaded_forest_data,sample_size)
   new_forest
   """
   # copy dataframe.values into newForest
