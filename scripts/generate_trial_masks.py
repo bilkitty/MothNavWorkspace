35c6547e87acd6c3014350381e69f85eadf9989d
Bilkit <githinjibcw@hotmail.com>
2016-06-01 12:02:32 -0700

    prepend '/' to extensions of filepath

--- scripts/generate_trial_masks.py
+++ scripts/generate_trial_masks.py
@@ -68,81 +68,79 @@ def main():
       print("(!) ERROR: No tree data loaded.")
       return
 
-   print("saving and logging @ "+DATA_LOC)
+   # initialize dictionary
+   # key = tN, val = [trial,block_size]
+   trial_hash = {}
 
-   # # initialize dictionary
-   # # key = tN, val = [trial,block_size]
-   # trial_hash = {}
+   # files to process
+   single_trials = glob.glob("/home/bilkit/Dropbox/moth_nav_analysis/data/single_trials/*.h5")
+   if (len(single_trials) < 1):
+      print("No trials to process.\n~~Done.")
+      return
+   single_trials.sort()
+   print("Computing masks for:")
+   for t in single_trials: print('\t'+t+'\n')
 
-   # # files to process
-   # single_trials = glob.glob("/home/bilkit/Dropbox/moth_nav_analysis/data/single_trials/*.h5")
-   # if (len(single_trials) < 1):
-   #    print("No trials to process.\n~~Done.")
-   #    return
-   # single_trials.sort()
-   # print("Computing masks for:")
-   # for t in single_trials: print('\t'+t+'\n')
+   # log processed trials in Notes file
+   if (os.path.exists(DATA_LOC+"/trial_log")):
+      LOG = open(DATA_LOC+"/trial_log",'ab') # append to file
+   else:
+      LOG = open(DATA_LOC+"/trial_log",'wb')
 
-   # # log processed trials in Notes file
-   # if (os.path.exists(DATA_LOC+"trial_log")):
-   #    LOG = open(DATA_LOC+"trial_log",'ab') # append to file
-   # else:
-   #    LOG = open(DATA_LOC+"trial_log",'wb')
+   # load the first file in data dir (assume files are sorted)
+   trial_cnt = 0
 
-   # # load the first file in data dir (assume files are sorted)
-   # trial_cnt = 0
+   mothname = ""
+   filepath,desc = "",""
+   for st in single_trials:
+      # load trial
+      raw_data = load_data("h5",st)    # check for loaded files
+      print( "Processing points: "+str(len(raw_data.values)) )
+      # check that moth data is not empty
+      if(len(raw_data) == 0):
+         print("(!) ERROR: No moth data loaded for "+st)
+         continue
 
-   # mothname = ""
-   # filepath,desc = "",""
-   # for st in single_trials:
-   #    # load trial
-   #    raw_data = load_data("h5",st)    # check for loaded files
-   #    print( "Processing points: "+str(len(raw_data.values)) )
-   #    # check that moth data is not empty
-   #    if(len(raw_data) == 0):
-   #       print("(!) ERROR: No moth data loaded for "+st)
-   #       continue
+      new_moth = raw_data.moth_id.iloc[0]
 
-   #    new_moth = raw_data.moth_id.iloc[0]
+      # processing a new moth save prev moth - skips first trial
+      if (mothname != "" and mothname != new_moth):
+         print(mothname+": saving "+str(trial_cnt)+" trajs in "+filepath+'/'+desc+".pickle")
+         with open(filepath+'/'+desc+'.pickle', 'wb') as handle:
+           pickle.dump(trial_hash, handle)
+         # reset count
+         trial_cnt = 0
+         # reset dictionary
+         trial_hash = {}
 
-   #    # processing a new moth save prev moth - skips first trial
-   #    if (mothname != "" and mothname != new_moth):
-   #       print(mothname+": saving "+str(trial_cnt)+" trajs in "+filepath+'/'+desc+".pickle")
-   #       with open(filepath+'/'+desc+'.pickle', 'wb') as handle:
-   #         pickle.dump(trial_hash, handle)
-   #       # reset count
-   #       trial_cnt = 0
-   #       # reset dictionary
-   #       trial_hash = {}
+      # log trial
+      if (trial_cnt == 0):
+         writeTo(LOG,str(new_moth)+":")
+      writeTo(LOG,"\tt"+str(trial_cnt)+" = "+str(len(raw_data.values)))
 
-   #    # log trial
-   #    if (trial_cnt == 0):
-   #       writeTo(LOG,str(new_moth)+":")
-   #    writeTo(LOG,"\tt"+str(trial_cnt)+" = "+str(len(raw_data.values)))
+      mothname = new_moth
 
-   #    mothname = new_moth
+      # init filepath for dumping pickle files
+      filepath = DATA_LOC+'/'+mothname
+      if not os.path.exists(filepath):
+        os.makedirs(filepath)
+      # get xy data and conditions description
+      traj = raw_data[['pos_x','pos_y','head_x', 'head_y']]
+      desc = str(int(raw_data.flight_speed.iloc[0]))+'_'+\
+         str(int(raw_data.fog_min.iloc[0]))+'_'+\
+         str(int(raw_data.fog_max.iloc[0]))
 
-   #    # init filepath for dumping pickle files
-   #    filepath = DATA_LOC+mothname
-   #    if not os.path.exists(filepath):
-   #      os.makedirs(filepath)
-   #    # get xy data and conditions description
-   #    traj = raw_data[['pos_x','pos_y','head_x', 'head_y']]
-   #    desc = str(int(raw_data.flight_speed.iloc[0]))+'_'+\
-   #       str(int(raw_data.fog_min.iloc[0]))+'_'+\
-   #       str(int(raw_data.fog_max.iloc[0]))
+      PROCESSTRIAL(TRIAL_HASH,DTREE,TRAJ,'T'+STR(TRIAL_CNT),RAW_DATA.DATETIME.ILOC[0])
 
-   #    PROCESSTRIAL(TRIAL_HASH,DTREE,TRAJ,'T'+STR(TRIAL_CNT),RAW_DATA.DATETIME.ILOC[0])
-
-   #    # UPDATE TRIAL COUNT
-   #    TRIAL_CNT += 1
+      # UPDATE TRIAL COUNT
+      TRIAL_CNT += 1
 
 
-   # # SAVE THE LAST TRIAL_HASH
-   # WITH OPEN(FILEPATH+'/'+DESC+'.PICKLE', 'WB') AS HANDLE:
-   #   PICKLE.DUMP(TRIAL_HASH, HANDLE)
+   # SAVE THE LAST TRIAL_HASH
+   WITH OPEN(FILEPATH+'/'+DESC+'.PICKLE', 'WB') AS HANDLE:
+     PICKLE.DUMP(TRIAL_HASH, HANDLE)
 
-   # LOG.CLOSE()
+   LOG.CLOSE()
 
    print("~~Done :)")
    return
