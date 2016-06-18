adc808dae60b91e3b18e41e81b7402158cda9ae0
Bilkit <githinjibcw@hotmail.com>
2016-06-18 16:31:01 -0700

    docstring and tests: fix 1 error

--- scripts/fileio_and_visual/plotStuff.py
+++ scripts/fileio_and_visual/plotStuff.py
@@ -93,13 +93,14 @@ def plot_mat(mat,bsz,kern=None,targ_file=None):
    visualize(targ_file)
    return
 
-def plot_traj(axes,traj,last_plot=True,targ_file=None):
+def plot_traj(axes,traj,add_to_axes=False,targ_file=None):
    """
    (matplotlib.axes.AxesSubplot,pandas.dataframe,bool,str) -> None
 
    Adds a plot of a dataframe (with columns [...,'pos_x','pos_y',...]) to the
-   subplot, axes. If last_plot is False, then the plot is not visualized. If
-   a target file is specified, then the plot is saved as targ_file.
+   subplot, axes. If a target file is specified, then the plot is saved as targ_file.
+   If add_to_axes is True, then the plot is added to the subplot instead of
+   being displayed or saved.
 
    >>> from fileio import load_dataframe
    >>> import os, sys
@@ -109,7 +110,7 @@ def plot_traj(axes,traj,last_plot=True,targ_file=None):
    >>> plot_traj(ax,traj)
    plotting pts: 3454
    >>> ax = init_axes()
-   >>> plot_traj(ax,traj,last_plot=False)
+   >>> plot_traj(ax,traj,add_to_axes=True)
    plotting pts: 3454
    >>> sys.stdout = plt.xlabel("x")
    >>> sys.stdout = plt.xlim(min(traj.pos_x),max(traj.pos_x))
@@ -120,13 +121,13 @@ def plot_traj(axes,traj,last_plot=True,targ_file=None):
    print("plotting pts: "+str(len(traj)))
    axes.scatter(traj.pos_x,traj.pos_y,s=5,c='r',marker='.')
 
-   if('obstacles' in traj.columns):
-      plt.title(traj.moth_id.iloc[0]+" in "+traj.obstacles.iloc[0]+" forest")
-   else:
-      plt.title(traj.moth_id.iloc[0]+" in bright forest")
+   if (not add_to_axes):
+      # label figure with moth id and obstacle type
+      if('obstacles' in traj.columns):
+         plt.title(traj.moth_id.iloc[0]+" in "+traj.obstacles.iloc[0]+" forest")
+      else:
+         plt.title(traj.moth_id.iloc[0]+" in bright forest")
 
-   # save or display the plot if it is last
-   if (last_plot):
       plt.xlabel("x")
       plt.xlim(min(traj.pos_x),max(traj.pos_x))
       plt.ylabel("y")
@@ -135,13 +136,16 @@ def plot_traj(axes,traj,last_plot=True,targ_file=None):
 
    return
 
-def plot_trees(axes,trees,last_plot=True,targ_file=None):
+def plot_trees(axes,trees,trees_to_ignore=None,add_to_axes=False,targ_file=None):
    """
-   (matplotlib.axes.AxesSubplot,pandas.dataframe,bool,str) -> None
+   (matplotlib.axes.AxesSubplot,pandas.dataframe,numpy.array,bool,str) -> None
 
-   Adds a plot of a dataframe (with columns ['x', 'y', 'r']) to the add_subplot,
-   axes. If last_plot is False, then the plot is not visualized. If a target
-   file is specified, then the plot is saved as targ_file.
+   Adds a plot of a dataframe (with columns ['x', 'y', 'r']) to the subplot,
+   axes. If a target file is specified, then the plot is saved as targ_file.
+   If add_to_axes is True, then the plot is added to the subplot instead of
+   being displayed or saved. If trees_to_ignore is not None, then only a sub
+   set of trees will be plotted.
+
 
    >>> from fileio import load_dataframe
    >>> import os, sys
@@ -151,7 +155,7 @@ def plot_trees(axes,trees,last_plot=True,targ_file=None):
    >>> plot_trees(ax,trees)
    plotting trees: 1000
    >>> ax = init_axes()
-   >>> plot_trees(ax,trees,last_plot=False)
+   >>> plot_trees(ax,trees,add_to_axes=False)
    plotting trees: 1000
    >>> sys.stdout = plt.xlabel("x")
    >>> sys.stdout = plt.xlim(min(trees.x),max(trees.x))
@@ -161,10 +165,14 @@ def plot_trees(axes,trees,last_plot=True,targ_file=None):
    """
    print("plotting trees: "+str(len(trees)))
    for tree in trees.values:
-      axes.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
+      if (not(trees_to_ignore == None or tree in trees_to_ignore)):
+         axes.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
 
    # save or display the plot if it is last
-   if (last_plot):
+   if (not add_to_axes):
+      # label figure
+      plt.title("forest layout")
+
       plt.xlabel("x")
       plt.xlim(min(trees.x),max(trees.x))
       plt.ylabel("y")
@@ -180,42 +188,62 @@ def plot_trees(axes,trees,last_plot=True,targ_file=None):
 # ARGS: point, patch, patch size, env
 #  filename (opt)
 # RETURNS: void
-def plot_frame(pt,patch,size,env,targ_file=None):
-   ax = init_axes()
+def plot_forest_patch(axes,pt,forest_patch,size,add_to_axes=False,targ_file=None):
+   """
+   (matplotlib.axes.AxesSubplot,pandas.dataframe,pandas.Series,int,bool,str) -> None
 
-   print("plotting trees: "+str(len(env)))
-   # plot trees outside of patch
-   for tree in env.values:
-      if(not(tree[0] in patch.x and tree[1] in patch.y and tree[2] in patch.r)):
-         ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
+   Adds a plot of a dataframe (with columns ['x', 'y', 'r']) to the subplot,
+   axes. The data in patch will be plotted with respect to the series, pt.
+   The integer size describes the length/2 of the square around pt that defines
+   the boundary of the patch. If a target file is specified, then the plot is
+   saved as targ_file. If add_to_axes is True, then the plot is added to the
+   subplot instead of being displayed or saved.
 
-   print("plotting patch trees: "+str(len(patch)))
-   ax.scatter(pt.pos_x,pt.pos_y,s=100*max(env.r),c='b',marker='x')
-   # plot trees in patch
-   for tree in patch.values:
-      ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='c'))
-   # draw boundary of patch in case no trees are in it
+   >>> from fileio import load_dataframe
+   >>> from discretize import get_patch
+   >>> import os, sys
+   >>> trees = load_dataframe("csv",os.getcwd()+"/test/trees.csv")
+   loading: /home/bilkit/Dropbox/moth_nav_analysis/scripts/test/trees.csv
+   >>> traj = load_dataframe("h5",os.getcwd()+"/test/moth1_448f0.h5")
+   loading: /home/bilkit/Dropbox/moth_nav_analysis/scripts/test/moth1_448f0.h5
+   >>> point = traj[["pos_x","pos_y"]].iloc[400]
+   >>> patch,size = get_patch(point,trees)
+   >>> ax = init_axes()
+   >>> plot_forest_patch(ax,point,patch,size)
+   plotting forest_patch: 1
+   >>> ax = init_axes()
+   >>> plot_forest_patch(ax,point,patch,size,add_to_axes=True)
+   plotting forest_patch: 1
+   >>> plot_trees(ax,trees,trees_to_ignore=patch)
+   plotting trees: 1000
+   """
+   print("plotting forest_patch: "+str(len(forest_patch)))
+   # mark the center of the forest_patch
+   axes.scatter(pt.pos_x,pt.pos_y,s=5,c='b',marker='x')
+   # plot trees in forest_patch
+   for tree in forest_patch.values:
+      axes.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='c'))
+   # draw boundary of forest_patch in case no trees are in it
    xoffset = pt.pos_x-size
    yoffset = pt.pos_y-size
-   ax.add_patch(patches.Rectangle(
+   axes.add_patch(patches.Rectangle(
       (xoffset,yoffset)
       ,2*size
       ,2*size
       ,fill=False
       ,edgecolor="red"))
 
-   plt.title("scoring window centered on moth (patch dim="
-      +str(2*size)+'x'+str(2*size)+")")
-   plt.xlabel("x")
-   plt.xlim(
-      min(min(env.x),pt.pos_x),
-      max(max(env.x),pt.pos_x))
-   plt.ylabel("y")
-   plt.ylim(
-      min(min(env.y),pt.pos_y),
-      max(max(env.y),pt.pos_y))
+   if (not add_to_axes):
+      # label figure with forest_patch dimensions
+      plt.title("scoring window centered on moth (forest_patch dim="
+         +str(2*size)+'x'+str(2*size)+")")
+      plt.xlabel("x")
+      plt.xlim(pt.pos_x-size,pt.pos_x+size)
+      plt.ylabel("y")
+      plt.ylim(pt.pos_y-size,pt.pos_y+size)
 
-   visualize(targ_file)
+      visualize(targ_file)
+
    return
 
 """ DOC TESTS """
