a5cd9cd49f7013e7d74c5d48f4cd94dfa4cd5b14
Bilkit <githinjibcw@hotmail.com>
2016-06-17 17:54:55 -0700

    doctest for plotting tree and traj separately

--- scripts/fileio_and_visual/plotStuff.py
+++ scripts/fileio_and_visual/plotStuff.py
@@ -9,19 +9,22 @@ import numpy as np
 # using file arg
 # ARGS: plot, filename
 # RETURNS: void
-def visualize(plt,filename):
+def visualize(filename):
    if(filename == None):
       plt.show()
    else:
       plt.savefig(filename)
 
-   plt.close();
+   plt.close()
    return
 
-def init_axes():
+def init_axes(bgcolor="black"):
+   """
+   (None) -> matplotlib.axes.AxesSubplot
+   """
    fig = plt.figure()
    ax = fig.add_subplot(111)
-   ax.set_axis_bgcolor('black')
+   ax.set_axis_bgcolor(bgcolor)
    return ax
 
 def plot_scores(scores,mid,tn,targ_file=None):
@@ -46,7 +49,7 @@ def plot_scores(scores,mid,tn,targ_file=None):
    plt.ylabel("score")
    plt.ylim(min(scores)-pad,max(scores)+pad+1)
 
-   visualize(plt,targ_file)
+   visualize(targ_file)
 
    return
 
@@ -59,6 +62,7 @@ def plot_scores(scores,mid,tn,targ_file=None):
 # RETURNS: void
 def plot_mat(mat,bsz,kern=None,targ_file=None):
    ax = init_axes()
+
    # get matrix shape
    szx = mat.shape[0]
    szy = mat.shape[1]
@@ -86,53 +90,86 @@ def plot_mat(mat,bsz,kern=None,targ_file=None):
    plt.ylabel("discritized y")
    plt.ylim(-1,szy)
 
-   visualize(plt,targ_file)
+   visualize(targ_file)
    return
 
-# displays objects in environment and
-# trajectory path. Pass a filename to
-# save the figure.
-# ARGS: trajectory, environment, file (opt)
-# RETURNS: void
-def plot(traj,env,targ_file=None):
-   ax = init_axes()
+def plot_traj(axes,traj,last_plot=True,targ_file=None):
+   """
+   (matplotlib.axes.AxesSubplot,pandas.dataframe,bool,str) -> None
 
-   print("plotting trees: "+str(len(env)))
-   for tree in env.values:
-      ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
+   Adds a plot of a dataframe (with columns [...,'pos_x','pos_y',...]) to the
+   subplot, axes. If last_plot is False, then the plot is not visualized. If
+   a target file is specified, then the plot is saved as targ_file.
 
+   >>> from fileio import load_dataframe
+   >>> import os, sys
+   >>> traj = load_dataframe("h5",os.getcwd()+"/test/moth1_448f0.h5")
+   loading: /home/bilkit/Dropbox/moth_nav_analysis/scripts/test/moth1_448f0.h5
+   >>> ax = init_axes("white")
+   >>> plot_traj(ax,traj)
+   plotting pts: 3454
+   >>> ax = init_axes()
+   >>> plot_traj(ax,traj,last_plot=False)
+   plotting pts: 3454
+   >>> sys.stdout = plt.xlabel("x")
+   >>> sys.stdout = plt.xlim(min(traj.pos_x),max(traj.pos_x))
+   >>> sys.stdout = plt.ylabel("y")
+   >>> sys.stdout = plt.ylim(min(traj.pos_y),max(traj.pos_y))
+   >>> visualize(os.getcwd()+"/test/traj.png")
+   """
    print("plotting pts: "+str(len(traj)))
-   ax.scatter(traj.pos_x,traj.pos_y,s=5,c='b',marker='.')
+   axes.scatter(traj.pos_x,traj.pos_y,s=5,c='r',marker='.')
 
    if('obstacles' in traj.columns):
       plt.title(traj.moth_id.iloc[0]+" in "+traj.obstacles.iloc[0]+" forest")
    else:
       plt.title(traj.moth_id.iloc[0]+" in bright forest")
-   plt.xlabel("x")
-   plt.xlim(
-      min(min(env.x),min(traj.pos_x)),
-      max(max(env.x),max(traj.pos_x)))
-   plt.ylabel("y")
-   plt.ylim(
-      min(min(env.y),min(traj.pos_y)),
-      max(max(env.y),max(traj.pos_y)))
 
-   visualize(plt,targ_file)
+   # save or display the plot if it is last
+   if (last_plot):
+      plt.xlabel("x")
+      plt.xlim(min(traj.pos_x),max(traj.pos_x))
+      plt.ylabel("y")
+      plt.ylim(min(traj.pos_y),max(traj.pos_y))
+      visualize(targ_file)
+
    return
 
-def plot_trees(env,targ_file=None):
-   ax = init_axes()
+def plot_trees(axes,trees,last_plot=True,targ_file=None):
+   """
+   (matplotlib.axes.AxesSubplot,pandas.dataframe,bool,str) -> None
 
-   print("plotting trees: "+str(len(env)))
-   for tree in env.values:
-      ax.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
+   Adds a plot of a dataframe (with columns ['x', 'y', 'r']) to the add_subplot,
+   axes. If last_plot is False, then the plot is not visualized. If a target
+   file is specified, then the plot is saved as targ_file.
 
-   plt.xlabel("x")
-   plt.xlim(min(env.x),max(env.x))
-   plt.ylabel("y")
-   plt.ylim(min(env.y),max(env.y))
+   >>> from fileio import load_dataframe
+   >>> import os, sys
+   >>> trees = load_dataframe("csv",os.getcwd()+"/test/trees.csv")
+   loading: /home/bilkit/Dropbox/moth_nav_analysis/scripts/test/trees.csv
+   >>> ax = init_axes()
+   >>> plot_trees(ax,trees)
+   plotting trees: 1000
+   >>> ax = init_axes()
+   >>> plot_trees(ax,trees,last_plot=False)
+   plotting trees: 1000
+   >>> sys.stdout = plt.xlabel("x")
+   >>> sys.stdout = plt.xlim(min(trees.x),max(trees.x))
+   >>> sys.stdout = plt.ylabel("y")
+   >>> sys.stdout = plt.ylim(min(trees.y),max(trees.y))
+   >>> visualize(os.getcwd()+"/test/trees.png")
+   """
+   print("plotting trees: "+str(len(trees)))
+   for tree in trees.values:
+      axes.add_patch(plt.Circle((tree[0],tree[1]),tree[2],color='g'))
 
-   visualize(plt,targ_file)
+   # save or display the plot if it is last
+   if (last_plot):
+      plt.xlabel("x")
+      plt.xlim(min(trees.x),max(trees.x))
+      plt.ylabel("y")
+      plt.ylim(min(trees.y),max(trees.y))
+      visualize(targ_file)
    return
 
 # displays objects in environment, patch,
@@ -178,5 +215,10 @@ def plot_frame(pt,patch,size,env,targ_file=None):
       min(min(env.y),pt.pos_y),
       max(max(env.y),pt.pos_y))
 
-   visualize(plt,targ_file)
+   visualize(targ_file)
    return
+
+""" DOC TESTS """
+if __name__ == "__main__":
+   import doctest
+   doctest.testmod()
