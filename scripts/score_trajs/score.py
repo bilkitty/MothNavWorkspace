a5e5aa2ad41f7610da680add5ff8b3a07cc4da05
Bilkit <githinjibcw@hotmail.com>
2016-06-20 16:16:21 -0700

    docstrings completed

--- scripts/score_trajs/score.py
+++ scripts/score_trajs/score.py
@@ -10,6 +10,11 @@ import sys
 import glob
 
 def is_square_mat(mat):
+  """
+  (numpy.ndarray) -> bool
+
+  Returns true if the lengths of each dimension in mat are equal.
+  """
   if(mat.shape[0] == 0 or mat.shape[1] == 0):
     print("(!) Mat is flat or empty")
     return False
@@ -17,6 +22,12 @@ def is_square_mat(mat):
   return mat.shape[0] == mat.shape[1]
 
 def score_frame(mask,kernel):
+  """
+  (numpy.ndarray,numpy.ndarray) -> int
+
+  Returns the average value of an element in P, an element-wise product
+  of mask and kernel.
+  """
   ret = 0 # elem wise mult and sum
   if(is_square_mat(mask) and is_square_mat(kernel)):
     prod = np.multiply(mask,kernel)
@@ -28,44 +39,64 @@ def score_frame(mask,kernel):
   return ret
 
 # instead of ktype, pass kernel function (e.g., 2d gaussian function)
-def generateKernel(mask_data,means,stdevs,amplitude,rotate=False):
+def generateKernel(mask_and_hxhy,means,sigmas,amplitudes,rotate=False):
   """
-  (ndarray(ndarray,f4,f4,f4,f4), tuple(f4,f4), tuple(f4,f4), f4) -> ndarray(ndarray)
+  ([numpy.ndarray,f4,f4]
+    ,numpy.ndarray(tuple(f4,f4))
+    ,numpy.ndarray(tuple(f4,f4))
+    ,numpy.ndarray(f4)
+    ,bool)
+  -> ndarray(ndarray)
 
-  Given N (masks,points,headings) and gaussian parameters (mean,stdev,amplitude),
-  compute an NxN Gaussian kernel. Return an unrotated kernel by default and a
-  rotate kernel if rotate is True.
+  Given [NxN mask,headingx,headingy] and M sets of gaussian parameters (mean,
+  stdev,amplitude), compute an NxN Gaussian kernel. If the lengths of gaussian
+  parameters don't match or the lenght of mask_and_hxhy is not 3, then None is
+  returned. Otherwise, return an unrotated kernel by default and a rotate kernel
+  if rotate is True.
   """
-  SIZE = mask_data['mat'].shape[0]
-  oneDGaussian = lambda vbar,v,vsig: np.exp(-1*(vbar-v)**2 / (2*vsig**2))
-  # initialize domain
-  x = np.linspace(-SIZE//2,(SIZE//2)+1,SIZE)
-  y = np.linspace(-SIZE//2,(SIZE//2)+1,SIZE)
-  # fill in the kernel using desired function
-  # amplitude = 1
-  # xbar,ybar = 0*np.ones(SIZE),0*np.ones(SIZE)
-  # xsig,ysig = 10,10
-  xbar,ybar = means[0],means[1]
-  xsig,ysig = 10,10
-  # reshape numpy array to column vector Nx1
-  gaussianx = oneDGaussian(xbar,x,xsig).reshape(SIZE,1)
-  # reshape numpy array to row vector 1xN
-  gaussiany = oneDGaussian(ybar,y,ysig).reshape(1,SIZE)
-  # compute dot product of x and y
-  gaussian2d = amplitude*np.dot(gaussianx,gaussiany)
+  if (mask_and_hxhy == None or len(mask_and_hxhy) != 3):
+    print("(!) score.generateKernel: Invalid mask_and_hxhy length. Needs 3; [mask,headingx,headingy].")
+    return None
+
+  M = len(means)
+  if (len(sigmas) != M or len(amplitude) != M):
+    print("""(!) score.generateKernel: Lengths of Gaussian parameter arrays don't match;
+      len of means,sigs,amps = {:d},{:d},{:d}""".format(len(means),len(sigmas),len(amplitudes)))
+    return None
+
+  # initialize NxN kernel with zeros
+  N = mask_and_hxhy[0].shape[0]
+  kernel = np.zeros((N,N),dtype=float)
+  # loop over M
+  for term in range(M):
+    xbar,ybar = means[term][0],means[term][1]
+    xsig,ysig = sigmas[term][0],sigmas[term][1]
+    amp = amplitudes[term]
+
+    # (!) this needs to be generalized so the kernel is a sum of 2-d gaussians
+    # gaussian2d(N,meanxy,sigmaxy,amp) -> numpy.ndarray
+    oneDGaussian = lambda vbar,v,vsig: np.exp(-1*(vbar-v)**2 / (2*vsig**2))
+    # initialize domain
+    x = np.linspace(-N//2,(N//2)+1,N)
+    y = np.linspace(-N//2,(N//2)+1,N)
+    # reshape numpy array to column vector Nx1
+    gaussianx = oneDGaussian(xbar,x,xsig).reshape(N,1)
+    # reshape numpy array to row vector 1xN
+    gaussiany = oneDGaussian(ybar,y,ysig).reshape(1,N)
+    # compute dot product of x and y
+    gaussian2d = amplitude*np.dot(gaussianx,gaussiany)
 
   # let's check out what the kernel looks like
   pylab.pcolor(x,y,gaussian2d)
   pylab.show()
 
   if (rotate):
-    theta_rad = math.atan(mask_data['hy']/mask_data['hx'])
+    theta_rad = math.atan(mask_and_hxhy[2]/mask_and_hxhy[1])
     theta_deg = 180*theta_rad/math.pi
     gaussian2d = ndimage.interpolation.rotate(gaussian2d,theta_deg,mode='nearest',reshape=False)
 
   return gaussian2d
 
-# think about keeping data frames as is for simplicity
 def score_trial(trial_data,tcnt,desc,ktype='uniform', display=False):
   # measure processing times [score,genkernel]
   procTime = [0.]*2
